803ff0054140d639d26d1933daf44dd29e924bd5
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-17 01:22:20 +0200

    Remove redundant fields from account ViewModel

--- Citizen/Models/ManageViewModels/IndexViewModel.cs
+++ Citizen/Models/ManageViewModels/IndexViewModel.cs
@@ -10,12 +10,6 @@ namespace Citizen.Models.ManageViewModels
     {
         public bool HasPassword { get; set; }
 
-        public IList<UserLoginInfo> Logins { get; set; }
-
-        public string PhoneNumber { get; set; }
-
-        public bool TwoFactor { get; set; }
-
         public bool BrowserRemembered { get; set; }
     }
 }
