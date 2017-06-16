7ded2ab45c64acd1dc9720ec0290292c1ff3a224
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-17 01:47:59 +0200

    Remove SendSms from MessageServices

--- Citizen/Services/MessageServices.cs
+++ Citizen/Services/MessageServices.cs
@@ -15,11 +15,5 @@ namespace Citizen.Services
             // Plug in your email service here to send an email.
             return Task.FromResult(0);
         }
-
-        public Task SendSmsAsync(string number, string message)
-        {
-            // Plug in your SMS service here to send a text message.
-            return Task.FromResult(0);
-        }
     }
 }
