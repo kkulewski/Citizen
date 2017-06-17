7ec08ea81d4ef6fec24db8414967720fa4985e2f
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-17 02:09:14 +0200

    Add name to User

--- Citizen/Models/AccountViewModels/RegisterViewModel.cs
+++ Citizen/Models/AccountViewModels/RegisterViewModel.cs
@@ -14,6 +14,11 @@ namespace Citizen.Models.AccountViewModels
         public string Email { get; set; }
 
         [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
