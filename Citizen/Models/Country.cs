98deefaf33111cc9dec515800d15a1729e8f6238
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-17 03:25:38 +0200

    Add Country model

---
+++ Citizen/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models
+{
+    public class Country
+    {
+        [StringLength(2, MinimumLength = 50)]
+        public string Name;
+
+        [StringLength(2, MinimumLength = 50)]
+        public string Capital;
+
+        [StringLength(3, MinimumLength = 3)]
+        public string CurrencyCode;
+    }
+}
