c9784a439d18907e41a70b20623c424dc1242eec
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-18 16:03:18 +0200

    Add ChangeCountryViewModel

---
+++ Citizen/Models/ManageViewModels/ChangeCountryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models.ManageViewModels
+{
+    public class ChangeCountryViewModel
+    {
+        [Display(Name = "Money")]
+        [DataType(DataType.Currency)]
+        public decimal Money { get; set; }
+
+        [Required]
+        [Display(Name = "Country ID")]
+        public int CountryId { get; set; }
+
+        [Display(Name = "Country")]
+        public List<Country> CountryList { get; set; }
+    }
+}
