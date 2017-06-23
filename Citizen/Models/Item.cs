5d7cce41904b473d09dcda7a0c1aff3760fc0dad
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-23 02:43:04 +0200

    Add item abstract class

---
+++ Citizen/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models
+{
+    public abstract class Item
+    {
+        public string Name;
+
+        public int Amount;
+    }
+}
