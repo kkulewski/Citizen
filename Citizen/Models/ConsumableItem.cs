2f204641b2567a609a8b1110b8580758dff87b9c
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-23 02:48:18 +0200

    Add ConsumableItem

---
+++ Citizen/Models/ConsumableItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models
+{
+    public class ConsumableItem : Item
+    {
+        public int EnergyRecoverAmount;
+    }
+}
