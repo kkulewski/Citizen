58e6db81113b11a504bd17ee82fd3212a0c5c2d0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-24 22:34:03 +0200

    Add FoodItem

---
+++ Citizen/Models/Items/FoodItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models.Items
+{
+    public class FoodItem
+    {
+        public int FoodItemId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public string ApplicationUserId { get; set; }
+    }
+}
