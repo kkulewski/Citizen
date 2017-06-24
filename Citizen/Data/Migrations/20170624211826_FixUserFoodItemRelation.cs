a2aa1decabf86a3337452f2932ff96b87fef00ba
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-25 00:09:11 +0200

    Fix User - FoodItem relation

---
+++ Citizen/Data/Migrations/20170624211826_FixUserFoodItemRelation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Citizen.Data.Migrations
+{
+    public partial class FixUserFoodItemRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FoodItemId",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FoodItemId",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
