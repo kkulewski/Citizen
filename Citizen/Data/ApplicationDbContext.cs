08036cf4d1e1455ca7d92c24fdcac098de29070e
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-27 23:05:43 +0200

    Add user storage

--- Citizen/Data/ApplicationDbContext.cs
+++ Citizen/Data/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace Citizen.Data
         public DbSet<Country> Country { get; set; }
         [NotMapped]
         public DbSet<TimeEvent> TimeEvents { get; set; }
+        [NotMapped]
+        public DbSet<UserStorage> UserStorage { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -38,6 +40,8 @@ namespace Citizen.Data
             builder.Entity<TimeEvent>()
                 .HasAlternateKey(c => c.Name)
                 .HasName("AlternateKey_TimeEventName");
+
+            builder.Entity<UserStorage>();
 
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
---
+++ Citizen/Models/UserStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models
+{
+    public class UserStorage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+
+        public int GrainAmount { get; set; }
+
+        public int FoodAmount { get; set; }
+    }
+}
