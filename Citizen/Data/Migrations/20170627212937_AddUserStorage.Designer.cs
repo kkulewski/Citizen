af278b4a9ae4c26e200b35495b076d4545c5125c
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-06-27 23:31:42 +0200

    Add UserStorage migration

--- Citizen/Data/Migrations/20170627212937_AddUserStorage.Designer.cs
+++ Citizen/Data/Migrations/20170627212937_AddUserStorage.Designer.cs
@@ -8,9 +8,10 @@ using Citizen.Data;
 namespace Citizen.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170627212937_AddUserStorage")]
+    partial class AddUserStorage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
---
+++ Citizen/Data/Migrations/20170627212937_AddUserStorage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Citizen.Data.Migrations
+{
+    public partial class AddUserStorage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserStorage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    FoodAmount = table.Column<int>(nullable: false),
+                    GrainAmount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserStorage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserStorage_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserStorage_ApplicationUserId",
+                table: "UserStorage",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserStorage");
+        }
+    }
+}
