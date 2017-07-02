f456828173df6c3f4ba8a8bb0ab1a6cc236a8fe2
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-02 11:48:10 +0200

    Add storage viewModel

---
+++ Citizen/Models/CitizenViewModels/UserStorageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models.CitizenViewModels
+{
+    public class UserStorageViewModel
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Money")]
+        [DataType(DataType.Currency)]
+        public decimal Money { get; set; }
+
+        [Display(Name = "Country")]
+        public Country Country { get; set; }
+
+        [Display(Name = "Food")]
+        public int Food { get; set; }
+
+        [Display(Name = "Grain")]
+        public int Grain { get; set; }
+    }
+}
