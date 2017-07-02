4570d89137da4985f0a78d8856c1dae798f03061
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-02 11:16:37 +0200

    Add profile ViewModel

---
+++ Citizen/Models/CitizenViewModels/ProfileViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Citizen.Models.CitizenViewModels
+{
+    public class ProfileViewModel
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Energy")]
+        [Range(0, 1000)]
+        public int Energy { get; set; }
+
+        [Display(Name = "Energy to restore")]
+        [Range(0, 1000)]
+        public int EnergyRestore { get; set; }
+
+        [Display(Name = "Money")]
+        [DataType(DataType.Currency)]
+        public decimal Money { get; set; }
+
+        [Display(Name = "Country")]
+        public Country Country { get; set; }
+    }
+}
