29285a76dd87c103da87e6e79213a8dd643903a9
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-06 00:16:09 +0200

    Add DeleteMarketplaceOffer viewModel

---
+++ Citizen/Models/MarketplaceViewModels/DeleteMarketplaceOfferViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models.MarketplaceViewModels
+{
+    public class DeleteMarketplaceOfferViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ItemType ItemType { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
