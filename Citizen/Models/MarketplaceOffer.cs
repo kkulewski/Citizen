6a044cfd6cf12032ed53146a8ad808e6aa68eec7
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-05 17:46:09 +0200

    Add marketplace offer

---
+++ Citizen/Models/MarketplaceOffer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models
+{
+    public class MarketplaceOffer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public Item Item { get; set; }
+
+        public int Amount { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
