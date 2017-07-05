f24263a65791a0181ad8a74eb6e0fc89761968b3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-05 20:59:27 +0200

    Add EditOffer viewModel

---
+++ Citizen/Models/MarketplaceViewModels/EditMarketplaceOfferViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models.MarketplaceViewModels
+{
+    public class EditMarketplaceOfferViewModel
+    {
+        [Required]
+        public ItemType ItemType { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
