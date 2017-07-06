3b88c81307aa7731036365b1fe0eaa6e65720efe
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-06 23:33:23 +0200

    Add ApplicationUser repository

---
+++ Citizen/DAL/ApplicationUserRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Citizen.Data;
+using Citizen.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Citizen.DAL
+{
+    public class ApplicationUserRepo : IApplicationUserRepo
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ApplicationUserRepo(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<ApplicationUser> GetApplicationUsers()
+        {
+            return _dbContext.ApplicationUsers
+                .Include(p => p.Items)
+                .Include(p => p.MarketplaceOffers)
+                .Include(p => p.Country)
+                .Include(p => p.UserStorage);
+        }
+
+        public ApplicationUser GetApplicationUserById(string id)
+        {
+            return GetApplicationUsers().First(u => u.Id == id);
+        }
+    }
+}
---
+++ Citizen/DAL/IApplicationUserRepo.cs
@@ -0,0 +1,15 @@
+﻿using Citizen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.DAL
+{
+    public interface IApplicationUserRepo
+    {
+        IEnumerable<ApplicationUser> GetApplicationUsers();
+
+        ApplicationUser GetApplicationUserById(string id);
+    }
+}
