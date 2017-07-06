48351cfdb0ed348b8de5ffcdc362debdfb68aaee
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-06 23:33:39 +0200

    Add generic repository

---
+++ Citizen/DAL/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.DAL
+{
+    public class Repository : IRepository
+    {
+        private IApplicationUserRepo _applicationUserRepo;
+
+        public Repository(IApplicationUserRepo applicationUserRepo)
+        {
+            _applicationUserRepo = applicationUserRepo;
+        }
+
+        public IApplicationUserRepo ApplicationUserRepo() => _applicationUserRepo;
+    }
+}
