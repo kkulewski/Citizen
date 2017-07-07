f986af020ef5ec6f3ff89f4b47fd019a4c74d560
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-07 21:58:15 +0200

    Make ActionStatus a class

--- Citizen/Models/ActionStatus.cs
+++ Citizen/Models/ActionStatus.cs
@@ -1,9 +1,21 @@
 ﻿namespace Citizen.Models
 {
-    public struct ActionStatus
+    public class ActionStatus
     {
         public bool Success;
 
         public string Message;
+
+        public ActionStatus(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public ActionStatus()
+        {
+            Success = false;
+            Message = string.Empty;
+        }
     }
 }
