4370b1a1f65896fd69609643703449ca4bb7468c
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-14 19:47:47 +0200

    Add companies update migration

---
+++ Citizen/Data/Migrations/20170714174406_AddProductAndSourceToUser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Citizen.Data.Migrations
+{
+    public partial class AddProductAndSourceToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Companies",
+                newName: "MaxEmployments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Product",
+                table: "Companies",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Source",
+                table: "Companies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Product",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "Companies");
+
+            migrationBuilder.RenameColumn(
+                name: "MaxEmployments",
+                table: "Companies",
+                newName: "Type");
+        }
+    }
+}
