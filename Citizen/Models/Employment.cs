ff337989e79f04dcc87ac7ed3863276d5eae81dd
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-14 11:39:46 +0200

    Add employment model

---
+++ Citizen/Models/Employment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models
+{
+    public class Employment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Company Company { get; set; }
+
+        [ForeignKey("Company")]
+        public int CompanyId { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public int DaysWorker { get; set; }
+    }
+}
