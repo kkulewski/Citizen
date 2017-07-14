6c51a11d4fb8a3218ea11cf68104512772262a1b
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-15 01:45:08 +0200

    Add AddJobOffer view model

---
+++ Citizen/Models/WorkViewModels/AddJobOfferViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Citizen.Models.MarketplaceViewModels
+{
+    public class AddJobOfferViewModel
+    {
+        [Required]
+        public int CompanyId { get; set; }
+
+        public Company Company { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+    }
+}
