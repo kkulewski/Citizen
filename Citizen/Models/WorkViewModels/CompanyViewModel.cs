4c1f7b575465a5947948baf30d322b989de15f1b
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-14 20:38:14 +0200

    Add Company view model

---
+++ Citizen/Models/WorkViewModels/CompanyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Citizen.Models.WorkViewModels
+{
+    public class CompanyViewModel
+    {
+        public string Name { get; set; }
+
+        public ItemType Product { get; set; }
+
+        public ItemType Source { get; set; }
+
+        public int MaxEmployments { get; set; }
+
+        public List<Employment> Employments { get; set; }
+
+        public List<JobOffer> JobOffers { get; set; }
+    }
+}
