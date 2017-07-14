1ff7f10f88242e9044224e1989c991eea4ce7a09
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-14 11:37:02 +0200

    Add job offer model

---
+++ Citizen/Models/JobOffer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models
+{
+    public class JobOffer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Company Company { get; set; }
+
+        [ForeignKey("Company")]
+        public int CompanyId { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
