0ce403b52a7354890024bcd3c95caa4f2e2b4dd5
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-14 20:06:36 +0200

    Add CreateCompany view model

---
+++ Citizen/Models/WorkViewModels/CreateCompanyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citizen.Models.MarketplaceViewModels
+{
+    public class CreateCompanyViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public ItemType CompanyType { get; set; }
+
+        public IEnumerable<ItemType> CompanyTypes { get; set; }
+    }
+}
