3bfc7f1f73bed524a3ab302d68824cebac0d195f
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-15 02:33:26 +0200

    Add DeleteCompany view model

---
+++ Citizen/Models/WorkViewModels/DeleteCompanyViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Citizen.Models.WorkViewModels
+{
+    public class DeleteCompanyViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public ItemType Product { get; set; }
+
+        [Required]
+        public ItemType Source { get; set; }
+    }
+}
