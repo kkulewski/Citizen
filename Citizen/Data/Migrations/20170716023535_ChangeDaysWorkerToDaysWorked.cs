4edaf9735bbe52fc15a54a9dcf57ac610c9adcd9
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-16 04:36:20 +0200

    Add property name typo fix migration

---
+++ Citizen/Data/Migrations/20170716023535_ChangeDaysWorkerToDaysWorked.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Citizen.Data.Migrations
+{
+    public partial class ChangeDaysWorkerToDaysWorked : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DaysWorker",
+                table: "Employments",
+                newName: "DaysWorked");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DaysWorked",
+                table: "Employments",
+                newName: "DaysWorker");
+        }
+    }
+}
