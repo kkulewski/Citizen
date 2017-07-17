b01f887effbc7d1bfcb390993d3cbd059b14c75e
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-18 01:13:12 +0200

    Add concurrency timestamp migration

--- Citizen/Data/Migrations/20170717231208_AddConcurrencyTimestamps.Designer.cs
+++ Citizen/Data/Migrations/20170717231208_AddConcurrencyTimestamps.Designer.cs
@@ -9,9 +9,10 @@ using Citizen.Models;
 namespace Citizen.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170717231208_AddConcurrencyTimestamps")]
+    partial class AddConcurrencyTimestamps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
---
+++ Citizen/Data/Migrations/20170717231208_AddConcurrencyTimestamps.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Citizen.Data.Migrations
+{
+    public partial class AddConcurrencyTimestamps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "UserStorage",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "TimeEvents",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "MarketplaceOffers",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "JobOffers",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Items",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Employments",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Companies",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "AspNetUsers",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "UserStorage");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "TimeEvents");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "MarketplaceOffers");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "JobOffers");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Employments");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "AspNetUsers");
+        }
+    }
+}
